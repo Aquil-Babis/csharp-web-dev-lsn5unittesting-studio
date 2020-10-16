25debe8030803be584444a6040f07409b1c00729
Aquil-Babis <aquil0_0@yahoo.com>
2020-10-15 19:49:54 -0500

    Finished 12 Unit Tests.

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -13,5 +13,67 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+        [TestMethod]
+        public void OnlyBracketReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+        [TestMethod]
+        public void NoBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+        [TestMethod]
+        public void BracketsAroundOtherCharactersReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[LanchCode]"));
+        }
+        [TestMethod]
+        public void BracketsMidStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Lanch[Code]"));
+        }
+        [TestMethod]
+        public void BracketsBeforeString()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]LaunchCode"));
+        }
+        [TestMethod]
+        public void SoloBracketReturnsFalses()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+        //Test that Odd number of brackets returns false.
+        [TestMethod]
+        public void OddNumberOfBracketsReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[LanchCode"));
+        } 
+        //Test that wrong facing brackets returns false.
+        [TestMethod]
+        public void WrongFacingBracketsReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+        [TestMethod]
+        public void WrongFacingBracketsInStringReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("Launch][Code"));
+        }
+        [TestMethod]
+        public void NestedBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[Launch[Code]]"));
+        }
+        [TestMethod]
+        public void MismatchedBracketsShouldReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[)"));
+        }
+        [TestMethod]
+        public void MultipleBracketTypesUsedCorrectlyReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[Launch(Code){Uses Codes}]"));
+        }
     }
 }
